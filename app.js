371aeefc61bf964ee5a1381c8524212c45401a6b
Rene Ruotsalainen <rlruotsainen@gmail.com>
2021-02-03 21:43:52 +0200

    update

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
 const navBar = document.querySelector("header");
-
-window.onscroll = function() {scrollEffect()}
-
-function scrollEffect() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        navBar.classList.add("scrolled");
-    }
-    else {
-        navBar.classList.remove("scrolled");
-    }
-}
