adb90ed5809887a93928f554d0a84515019af68e
Rene Ruotsalainen <rlruotsalainen@gmail.com>
2021-02-04 17:54:54 +0200

    history bg

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const navBar = document.querySelector("header");
+const top_button = document.getElementById("b2t");
+
+document.onscroll = function() {scroll()}
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        top_button.style.display = "block";
+    }
+    else {
+        top_button.style.display = "none";
+    }   
+}
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
